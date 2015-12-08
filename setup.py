e98a4363153e6f0a2874a43feb0beb47ef60f6dd
Pierre Dulac <dulacpier@gmail.com>
2015-12-08 18:50:00 +0100

    Update the setup.py file
    inspired by django-rest-framework classy style

--- setup.py
+++ setup.py
@@ -1,53 +1,106 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import re
+import shutil
+import sys
 
+from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 requirements = [
     'click',
 ]
 
 test_requirements = [
-
 ]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('igenstrings')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('djangorestframework.egg-info')
+    sys.exit()
+
 
 setup(
     name='igenstrings',
-    version='0.1.0',
+    version=version,
+    url='https://github.com/dulaccc/igenstrings',
+    license="MIT",
     description="Enhance the genstrings command by adding merging capabilities",
     author="Pierre Dulac",
     author_email='pierre@dulaccc.me',
-    url='https://github.com/dulaccc/igenstrings',
-    packages=[
-        'igenstrings',
-    ],
-    package_dir={'igenstrings':
-                 'igenstrings'},
-    include_package_data=True,
+    packages=get_packages('igenstrings'),
+    package_dir=get_package_data('igenstrings'),
     install_requires=requirements,
-    license="ISCL",
+    include_package_data=True,
     zip_safe=False,
-    keywords='igenstrings',
     entry_points={
         'console_scripts': [
             'igenstrings = igenstrings.cli:main',
         ],
     },
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
