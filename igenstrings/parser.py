567ffb4ce3c505820ee062e6b6563fc0320e684f
Pierre Dulac <dulacpier@gmail.com>
2015-12-09 17:17:03 +0100

    Refactor the .strings file parser inspired by transifex one

--- igenstrings/parser.py
+++ igenstrings/parser.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from sys import argv
-from re import compile
 from copy import copy
 import os
 import io
+import re
 import shutil
 import optparse
 import logging
@@ -14,109 +14,144 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-re_translation = compile(r'^"((?:[^"]|\\")+)" = "((?:[^"]|\\")+)";(?:\n)?$')
-re_comment_single = compile(r'^/\*.*\*/$')
-re_comment_start = compile(r'^/\*.*$')
-re_comment_end = compile(r'^.*\*/$')
-
 STRINGS_FILE = 'Localizable.strings'
 
 
 class LocalizedString(object):
 
-    def __init__(self, comments, translation):
-        self.comments, self.translation = comments, translation
-        self.key, self.value = re_translation.match(self.translation).groups()
+    def __init__(self, key, value, comment="no comment"):
+        self.key = key
+        self.value = value
+        self.comment = comment
 
     def __unicode__(self):
-        return u"{}{}\n".format("".join(self.comments), self.translation)
+        return u'/*{}*/\n"{}" = "{}";\n'.format(self.comment, self.key, self.value)
 
 
 class LocalizedFile(object):
 
-    def __init__(self, filename=None):
-        self.strings = []
-        self.strings_d = {}
+    def __init__(self, strings=None):
+        """
+        :param: strings a list of `LocalizedString` instances
+        """
+        self.stringset = strings or []
 
-        if filename:
-            self.filename = filename
-            self.read()
-
-    def read(self):
-        with io.open(self.filename, encoding='utf16', mode='r') as f:
-            line = f.readline()
-            logger.debug(line)
-
-            i = 1
-            while line:
-                comments = [line]
-
-                if not re_comment_single.match(line):
-                    while line and not re_comment_end.match(line):
-                        line = f.readline()
-                        comments.append(line)
-
-                line = f.readline()
-                i += 1
-
-                # handle multi lines
-                while len(line) > 1 and line[-2] != u';':
-                    nextline = f.readline()
-                    if not nextline:
-                        nextline = '\n'
-                    line += nextline
-                    i += 1
-
-                logger.debug("%d %s" % (i, line.rstrip('\n')))
-                if line and re_translation.match(line):
-                    translation = line
-                else:
-                    logger.error("Line {} of file '{}' raising the exception: {}".format(i, self.filename, line))
-                    raise ValueError('invalid file')
-
-                line = f.readline()
-                i += 1
-                while line and line == u'\n':
-                    line = f.readline()
-                    i += 1
-
-                string = LocalizedString(comments, translation)
-                self.strings.append(string)
-                self.strings_d[string.key] = string
+    def read(self, input_filename):
+        with io.open(input_filename, encoding='utf16', mode='r') as f:
+            parser = AppleStringsParser()
+            content = f.read()
+            strings = parser.parse(content)
+            self.stringset = strings
 
     def save(self, output_filename):
+        if not self.stringset:
+            raise ValueError("Can't saved a file with no strings defined")
+
         with io.open(output_filename, encoding='utf16', mode='w') as f:
             # sort by key
-            self.strings.sort(key=lambda item: item.key)
-            for string in self.strings:
+            self.stringset.sort(key=lambda item: item.key)
+            for string in self.stringset:
                 f.write(string.__unicode__())
 
-    def merge_with(self, new):
+    def _get_stringsdict(self):
+        """Turns the stringset into a `dict`"""
+        s_dict = {}
+        for s in self.stringset:
+            s_dict[s.key] = s
+        return s_dict
+
+    def merge(self, other):
+        """
+        Merge the current file strings with the given other file strings
+        by taking any other string as the newer version
+
+        :param: other another instance of `LocalizedFile`
+        :return: the merged `LocalizedFile` instance
+        """
         merged = LocalizedFile()
-
-        for string in new.strings:
-            if string.key in self.strings_d:
-                new_string = copy(self.strings_d[string.key])
-                new_string.comments = string.comments
-                string = new_string
-
-            merged.strings.append(string)
-            merged.strings_d[string.key] = string
-
+        stringsdict = self._get_stringsdict()
+        for new_string in other.stringset:
+            if new_string.key in stringsdict:
+                old_string = copy(stringsdict[new_string.key])
+                old_string.comment = new_string.comment
+                new_string = old_string
+            merged.stringset.append(new_string)
         return merged
 
-    def update_with(self, new):
-        for string in new.strings:
-            if not self.strings_d.has_key(string.key):
-                self.strings.append(string)
-                self.strings_d[string.key] = string
+
+class AppleStringsParser(object):
+    """
+    Parser for Apple STRINGS translation files.
+
+    inspired by the transifex implementation
+    https://github.com/transifex/transifex/blob/master/transifex/resources/formats/strings.py
+
+    NB: Apple strings files *must* be encoded in UTF-16 encoding.
+    """
+
+    def _escape(self, s):
+        return s.replace('"', '\\"').replace('\n', r'\n').replace('\r', r'\r')
+
+    def _unescape_key(self, s):
+        return s.replace('\\\n', '')
+
+    def _unescape(self, s):
+        s = s.replace('\\\n', '')
+        return s.replace('\\"', '"').replace(r'\n', '\n').replace(r'\r', '\r')
+
+    def parse(self, content):
+        """
+        Parse an apple .strings file and create a stringset with all entries in the file.
+
+        See Apple spec for details.
+        https://developer.apple.com/library/ios/documentation/MacOSX/Conceptual/BPInternational/MaintaingYourOwnStringsFiles/MaintaingYourOwnStringsFiles.html
+        """
+        stringset = []
+
+        f = content
+        prefix = ""
+        if f.startswith(u'\ufeff'):
+            prefix = u'\ufeff'
+            f = f.lstrip(u'\ufeff')
+
+        #regex for finding all comments in a file
+        cp = r'(?:/\*(?P<comment>(?:[^*]|(?:\*+[^*/]))*\**)\*/)'
+        p = re.compile(r'(?:%s[ \t]*[\n]|[\r\n]|[\r]){0,1}(?P<line>(("(?P<key>[^"\\]*(?:\\.[^"\\]*)*)")|(?P<property>\w+))\s*=\s*"(?P<value>[^"\\]*(?:\\.[^"\\]*)*)"\s*;)'%cp, re.DOTALL|re.U)
+        #c = re.compile(r'\s*/\*(.|\s)*?\*/\s*', re.U)
+        c = re.compile(r'//[^\n]*\n|/\*(?:.|[\r\n])*?\*/', re.U)
+        ws = re.compile(r'\s+', re.U)
+
+        end = 0
+        start = 0
+        for i in p.finditer(f):
+            start = i.start('line')
+            end_ = i.end()
+            line = i.group('line')
+            key = i.group('key')
+            comment = i.group('comment') or ''
+            if not key:
+                key = i.group('property')
+            value = i.group('value')
+            while end < start:
+                m = c.match(f, end, start) or ws.match(f, end, start)
+                if not m or m.start() != end:
+                    raise StringsParseError("Invalid syntax: %s" %\
+                            f[end:start])
+                end = m.end()
+            end = end_
+            key = self._unescape_key(key)
+            stringset.append(LocalizedString(key, self._unescape(value), comment=comment))
+
+        return stringset
 
 
 def merge(merged_fname, old_fname, new_fname):
     try:
-        old = LocalizedFile(old_fname)
-        new = LocalizedFile(new_fname)
-        merged = old.merge_with(new)
+        old = LocalizedFile()
+        old.read(old_fname)
+        new = LocalizedFile()
+        new.read(new_fname)
+        merged = old.merge(new)
         merged.save(merged_fname)
     except Exception as inst:
         logger.error('Error: input files have invalid format : {}'.format(inst))
