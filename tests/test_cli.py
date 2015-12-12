3603056d9b555f79eb9f3230feb7172bf422a09e
Pierre Dulac <dulacpier@gmail.com>
2015-12-12 18:37:16 +0200

    Add tests for a new project configuration

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,21 +13,21 @@ def runner():
 
 
 def test_cli(runner):
-    result = runner.invoke(cli.main, ['tests/objc'])
+    result = runner.invoke(cli.main, ['tests/objc/existing'])
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip().startswith('Running the script')
 
 
 def test_cli_with_debug(runner):
-    result = runner.invoke(cli.main, ['--debug', 'tests/objc'])
+    result = runner.invoke(cli.main, ['--debug', 'tests/objc/existing'])
     assert not result.exception
     assert result.exit_code == 0
     assert 'Debug mode is on' in result.output
 
 
 # def test_cli_with_excluded_paths(runner):
-#     result = runner.invoke(cli.main, ['--excluded-path tests/objc', 'tests/objc'])
+#     result = runner.invoke(cli.main, ['--excluded-path tests/objc/existing', 'tests/objc/existing'])
 #     assert result.exit_code == 0
 #     assert not result.exception
 #     assert result.output.strip() == 'Hello, {{ cookiecutter.full_name.split()[0] }}.'
--- tests/test_merger.py
+++ tests/test_merger.py
@@ -8,24 +8,48 @@ test_igenstrings
 Tests for `igenstrings` module.
 """
 
+import os
 import unittest
 from codecs import open
 
+from py import path
+import pytest
+
 from igenstrings.merger import Merger
+
+
+class TestMerger(unittest.TestCase):
+    pass
 
 
 class TestMergerIntegration(unittest.TestCase):
 
     def setUp(self):
-        pass
+        super(TestMergerIntegration, self).setUp()
 
-    def tearDown(self):
-        pass
+    @pytest.fixture(autouse=True)
+    def initdir(self, tmpdir):
+        fixture_basename = 'tests/objc'
+        fixture_path = path.local(fixture_basename)
+        fixture_path.copy(tmpdir / fixture_basename)
+        tmpdir.chdir() # change to pytest-provided temporary directory
+
+    def test_create_localizable_strings(self):
+        merger = Merger('tests/objc/new', None)
+        self.assertTrue(not os.path.exists('tests/objc/new/en.lproj/Localizable.strings'))
+        self.assertTrue(not os.path.exists('tests/objc/new/fr.lproj/Localizable.strings'))
+        merger.merge_localized_strings()
+        self.assertTrue(os.path.exists('tests/objc/new/en.lproj/Localizable.strings'))
+        self.assertTrue(os.path.exists('tests/objc/new/fr.lproj/Localizable.strings'))
+        content = None
+        with open('tests/objc/new/en.lproj/Localizable.strings', encoding='utf16', mode='r') as en_locale_file:
+            content = en_locale_file.read()
+        self.assertIn('Hi', content)
 
     def test_keep_existing_translated_strings(self):
-        merger = Merger('tests/objc', None)
+        merger = Merger('tests/objc/existing', None)
         merger.merge_localized_strings()
         content = None
-        with open('tests/objc/fr.lproj/Localizable.strings', encoding='utf16', mode='r') as fr_locale_file:
+        with open('tests/objc/existing/fr.lproj/Localizable.strings', encoding='utf16', mode='r') as fr_locale_file:
             content = fr_locale_file.read()
         self.assertIn('Bonjour', content)
